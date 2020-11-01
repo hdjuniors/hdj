72aea19aa5910fa3a49206b41f32531740c5c644
JoelErmantraut1 <joelermantraut@gmail.com>
2020-11-01 14:59:22 -0300

    DEV. Corregidos los iconos de las listas, añadido contenido

--- pages/clases/script.js
+++ pages/clases/script.js
@@ -1,27 +1,31 @@
 (function($) {
 
   url_list = [
-    "https://www.youtube.com/embed/H7hrX7klZNs",
-    "https://www.youtube.com/embed/BStSwj7tC2I",
-    "https://www.youtube.com/embed/4dCxBYMmSN4",
-    "https://www.youtube.com/embed/po07o124rJA",
-    "https://www.youtube.com/embed/01GVoJqrThQ",
-    "https://www.youtube.com/embed/xzez0fYEEjk",
-    "https://www.youtube.com/embed/UDu0PLS7aUw",
-    "https://www.youtube.com/embed/pTAsVmeTCDI",
-    "https://www.youtube.com/embed/Kw-e3G7SK5k",
-    "https://www.youtube.com/embed/9g7LYJcl4Yo",
-    "https://www.youtube.com/embed/UppvY9o-sD0",
-    "https://www.youtube.com/embed/aI_jix2094w",
-    "https://www.youtube.com/embed/n94JOHFH6L8",
+    "https://www.youtube.com/embed/H7hrX7klZNs", // 1
+    "https://www.youtube.com/embed/BStSwj7tC2I", // 2
+    "https://www.youtube.com/embed/4dCxBYMmSN4", // 3
+    "https://www.youtube.com/embed/po07o124rJA", // 4
+    "https://www.youtube.com/embed/01GVoJqrThQ", // 5
+    "https://www.youtube.com/embed/xzez0fYEEjk", // 6
+    "https://www.youtube.com/embed/UDu0PLS7aUw", // 7
+    "https://www.youtube.com/embed/pTAsVmeTCDI", // 8
+    "https://www.youtube.com/embed/Kw-e3G7SK5k", // 9
+    "https://www.youtube.com/embed/9g7LYJcl4Yo", // 10
+    "https://www.youtube.com/embed/UppvY9o-sD0", // 11
+    "https://www.youtube.com/embed/aI_jix2094w", // 12
+    "https://www.youtube.com/embed/n94JOHFH6L8", // 13
     "",// ACA IRIA LA CLASE 14, DEL SEXTO MANDAMIENTO
-    "https://www.youtube.com/embed/wvUwDs6xmuk",
-    "https://www.youtube.com/embed/XioytlvQ_rY",
-    "https://www.youtube.com/embed/8Fyni8rI3X0",
-    "https://www.youtube.com/embed/I0x2K6RN61U",
-    "https://www.youtube.com/embed/YT43w2nS6qk",
-    "https://www.youtube.com/embed/Sda5CVw2CCw",
-    "https://www.youtube.com/embed/GDxDuJ2KOQs"
+    "https://www.youtube.com/embed/wvUwDs6xmuk", // 15
+    "https://www.youtube.com/embed/XioytlvQ_rY", // 16
+    "https://www.youtube.com/embed/8Fyni8rI3X0", // 17
+    "https://www.youtube.com/embed/I0x2K6RN61U", // 18
+    "https://www.youtube.com/embed/YT43w2nS6qk", // 19
+    "https://www.youtube.com/embed/Sda5CVw2CCw", // 20
+    "https://www.youtube.com/embed/GDxDuJ2KOQs", // 21
+    "https://www.youtube.com/embed/ikw7jhYIG60", // 22
+    "https://www.youtube.com/embed/FlBZvr3C6uo", // 23
+    "https://www.youtube.com/embed/yjVL5zubISQ", // 24
+    "https://www.youtube.com/embed/m9J7gyzxCy8", // 25
   ].reverse();
 
   $(document).ready(function() {
